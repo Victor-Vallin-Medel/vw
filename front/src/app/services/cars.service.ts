d6615681a040fd5adace4f2474fa5108bf673199
Daniel Molina <danmnvx@gmail.com>
2019-11-19 17:25:31 -0600

    Vars para mostrar autos en las vistas.

--- front/src/app/services/cars.service.ts
+++ front/src/app/services/cars.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Car } from '../models/car';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,28 +10,61 @@ export class CarsService {
 
   readonly URL: string = 'http://localhost:3004/cars/';
 
+  /* List (Array), Current (Interface) para mostrar en las vistas */
+  list: Observable<Car []>;
+  current: Observable<Car>;
+  
+  cars$: BehaviorSubject<Car []> = new BehaviorSubject<Car []>([]);
+
   constructor(private http:HttpClient) { }
   
+  /**
+   * Read all client cars.
+   * @param uid number
+   */
   getClientCars(uid: number) {
-    return this.http.get(`${this.URL}?car.Cliente_idCliente=${uid}`);
+    return this.http.get<Car []>(`${this.URL}?car.Cliente_idCliente=${uid}`).subscribe((partial: Car []) => {
+      this.cars$.next(partial);
+      this.list = this.cars$.asObservable();
+    });
   }
 
+  /**
+   * Read all cars.
+   */
   getCars() {
     return this.http.get(`${this.URL}`);
   }
 
-  getCar(id: string) {
+  /**
+   * Read one car.
+   * @param id number
+   */
+  getCar(id: number) {
     return this.http.get<Car>(`${this.URL}${id}`);
   }
 
+  /**
+   * Create a car.
+   * @param carDto Car
+   */
   postCar(carDto: Car) {
     return this.http.post(`${this.URL}`, carDto);
   }
 
-  putCar(id: string, status: number) {
+  /**
+   * Update a car.
+   * @param id      number
+   * @param status  number
+   */
+  putCar(id: number, status: number) {
     return this.http.put(`${this.URL}${id}`, { status: status});
   }
 
+  /**
+   * Delete a car.
+   * @param id number
+   */
   deleteCar(id: number) {
     return this.http.delete(`${this.URL}${id}`);
   }
